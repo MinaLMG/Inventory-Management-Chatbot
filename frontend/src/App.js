68e4b9a3623b439c37235605beffecff08285c1c
MinaLMG <beandro6@gmail.com>
2025-06-23 20:37:56 +0300

    created and tested the main layout for the catbot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import { useState } from "react";
+import {
+    MainContainer,
+    ChatContainer,
+    MessageList,
+    Message,
+    MessageInput,
+    TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [typing, setTyping] = useState(false);
+    const [messages, setMessages] = useState([
+        {
+            message: "hello, i'm chat gpt",
+            sender: "gpt",
+            direction: "incoming",
+        },
+    ]);
+    const handleSend = async (message) => {
+        const newMessage = {
+            message: message,
+            sender: "user",
+            direction: "outgoing",
+        };
+        const newMessages = [...messages, newMessage];
+        setMessages(newMessages);
+        setTyping(true);
+    };
+    return (
+        <div className="App-header">
+            <div
+                style={{
+                    position: "relative",
+                    height: "800px",
+                    width: "700px",
+                    margin: "auto",
+                }}
+            >
+                <MainContainer>
+                    <ChatContainer>
+                        <MessageList
+                            typingIndicator={
+                                typing ? (
+                                    <TypingIndicator content="Typing .." />
+                                ) : null
+                            }
+                        >
+                            {messages.map((msg, i) => (
+                                <Message key={i} model={msg}></Message>
+                            ))}
+                        </MessageList>
+                        <MessageInput
+                            placeholder="type message here ..."
+                            onSend={handleSend}
+                        ></MessageInput>
+                    </ChatContainer>
+                </MainContainer>
+            </div>
+        </div>
+    );
 }
 
 export default App;
